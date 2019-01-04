758b0fb99b615f6e64f743e364140d7c84a2b373
Fabian Deutsch <fabiand@fedoraproject.org>
2019-01-04 17:04:13 +0100

    Posts update
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- scripts/import-from-tumblr.sh
+++ scripts/import-from-tumblr.sh
@@ -1,7 +1,7 @@
 for F in $(ls -1 ../posts) ;
 do
   POSTID=${F%.md}
-  NEWFN=$(date --date="$(xmllint --xpath "string(tumblr/posts/post[@id=$POSTID]/@date)" ../posts/posts.xml )" +%Y-%m-%d-post.md)
+  NEWFN=$(date --date="$(xmllint --xpath "string(tumblr/posts/post[@id=$POSTID]/@date)" ../posts/posts.xml )" +%Y-%m-%d-$POSTID.md)
   TITLE=$(xmllint --xpath "string(tumblr/posts/post[@id=$POSTID]/regular-title)" ../posts/posts.xml)
   ( echo -e "---\nlayout: post\ntitle: \"${TITLE:-Today}\"\n---\n\n" ; cat ../posts/$F ) > ../_posts/$NEWFN
 done
